0c293a2c0a1bb700e77e555e80fa7dda3694105c
getNotesFS <sfmarmolg@gmail.com>
2020-10-23 21:09:07 -0500

    Contact ready and footer mod

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
  
     $('#openMAP').click(function(){ 
       $('#mapaLA').toggleClass("showLA"); 
-
+      $('.footerMainContainer').toggleClass("showLA");
+      $('.LINE_YELLOW').toggleClass("showLA");
+      $('footer').toggleClass('bg-darkLA');
     });
 
 
